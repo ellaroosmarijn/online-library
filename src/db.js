396172dcfa6adf99014a5afb4b620b3360acd89f
Ella Robinson <ellaroosmarijn@gmail.com>
2023-07-06 13:54:43 +1200

    fix: add user, password, port and host to connect to database

--- src/db.js
+++ src/db.js
@@ -1,6 +1,10 @@
 const { Client } = require("pg");
 const client = new Client({
   database: "postgres",
+  user: "postgres",
+  password: "4y7sV96vA9wv46VR",
+  port: 5432,
+  host: "localhost",
 });
 
 module.exports = client;
