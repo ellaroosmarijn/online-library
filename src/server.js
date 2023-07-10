aa989d545c1860ebac6fd3d98f1b20c8b0942679
Ella Robinson <ellaroosmarijn@gmail.com>
2023-07-10 14:54:16 +1200

    fix: move db.connect outside of function, and only call once - fixes error with creating too many db connections

--- src/server.js
+++ src/server.js
@@ -1,26 +1,31 @@
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
+import http from "http";
+import fs from "fs";
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
-const db = require("./db");
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const server = http.createServer((req, res) => {
+import db from "./db.js";
+const server = http.createServer();
+await db.connect();
+
+// TODO mchange to switch statement
+server.on("request", async (req, res) => {
   let filePath = path.resolve(__dirname + "/../public" + req.url);
 
   if (req.url === "/books" && req.method === "GET") {
     // Handle GET request for "/books"
-    db.connect()
-      .then(() => db.query("SELECT * FROM books"))
+    db.query("SELECT * FROM books")
       .then((result) => {
         res.setHeader("All-Books", "application/json");
         res.statusCode = 200;
         res.end(JSON.stringify(result.rows));
-        return db.end();
       })
       .catch((error) => {
         console.error("Error retrieving books:", error);
         res.statusCode = 500;
-        res.end(JSON.stringify({ error: "Error retrieving books" }));
+        res.end(JSON.stringify({ error: "Error retrieving books" })``);
       });
   } else if (req.url === "/books/add" && req.method === "POST") {
     // Handle POST request for "/books/add"
@@ -30,18 +35,14 @@ const server = http.createServer((req, res) => {
     });
     req.on("end", () => {
       const bookData = JSON.parse(body);
-      // Perform database operation to add the book
-      db.connect()
-        .then(() => {
-          db.query(
-            "INSERT INTO library (title, author, description) VALUES($1, $2, $3) RETURNING *",
-            [bookData.title, bookData.author, bookData.description]
-          );
-        })
+      // Perform db operation to add the book
+      db.query(
+        "INSERT INTO books (title, author, description) VALUES($1, $2, $3) RETURNING *",
+        [bookData.title, bookData.author, bookData.description]
+      )
         .then(() => {
           res.statusCode = 201;
           res.end(JSON.stringify({ message: "Book added successfully" }));
-          return db.end();
         })
         .catch((error) => {
           console.error("Error adding book:", error);
@@ -56,38 +57,31 @@ const server = http.createServer((req, res) => {
     req.on("data", (updatedBookData) => {
       body += updatedBookData;
     });
-    req.on("end", () => {
-      const bookData = JSON.parse(body);
-      // Perform database operation to update the book
-      db.connect()
-        .then(() => {
-          db.query(
-            "UPDATE books SET title = $1, author = $3, description = $2 WHERE book_id = $4",
-            [bookData.title, bookData.description, bookId]
-          );
-        })
-        .then(() => {
-          res.statusCode = 200;
-          res.end(JSON.stringify({ message: "Book updated successfully" }));
-          return db.end();
-        })
-        .catch((error) => {
-          console.error("Error updating book:", error);
-          res.statusCode = 500;
-          res.end(JSON.stringify({ error: "Error updating book" }));
-        });
-    });
-  } else if (req.url.match(/^\/books\/\d+$/) && req.method === "PATCH") {
-    // Handle DELETE request for "/books/:id"
-    const bookId = req.url.split("/")[2];
-    db.connect()
-      .then(() => {
-        return db.query("DELETE FROM library WHERE book_id = $1", [bookId]);
+    req
+      .on("end", () => {
+        const bookData = JSON.parse(body);
+        // Perform db operation to update the book
+        db.query(
+          "UPDATE books SET title = $1, author = $3, description = $2 WHERE book_id = $4",
+          [bookData.title, bookData.description, bookId]
+        );
       })
       .then(() => {
         res.statusCode = 200;
+        res.end(JSON.stringify({ message: "Book updated successfully" }));
+      })
+      .catch((error) => {
+        console.error("Error updating book:", error);
+        res.statusCode = 500;
+        res.end(JSON.stringify({ error: "Error updating book" }));
+      });
+  } else if (req.url.match(/^\/books\/\d+$/) && req.method === "PATCH") {
+    // Handle DELETE request for "/books/:id"
+    const bookId = req.url.split("/")[2];
+    db.query("DELETE FROM books WHERE book_id = $1", [bookId])
+      .then(() => {
+        res.statusCode = 200;
         res.end(JSON.stringify({ message: "Book deleted successfully" }));
-        return db.end();
       })
       .catch((error) => {
         console.error("Error deleting book:", error);
